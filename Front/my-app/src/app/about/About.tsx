db781fe3a6d677bd23b5740c0faae407d7ebbe74
AleFalces <alefalces@gmail.com>
2025-03-19 15:36:05 -0300

    firts component and route

---
+++ Front/my-app/src/app/about/About.tsx
@@ -0,0 +1,10 @@
+const AboutPage: React.FC = () => {
+  return (
+    <main>
+      <h1>Acerca de Nosotros</h1>
+      <p>Bienvenido a la página de información sobre nuestra empresa.</p>
+    </main>
+  );
+};
+
+export default AboutPage;
