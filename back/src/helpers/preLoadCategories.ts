9d8caa3e7a8ba1d46727abbffbb9627d2e07c46d
AleFalces <alefalces@gmail.com>
2025-04-21 11:04:13 -0300

    change productos

--- back/src/helpers/preLoadCategories.ts
+++ back/src/helpers/preLoadCategories.ts
@@ -3,23 +3,25 @@ import { Category } from "../entities/Category";
 import { CategoryRepository } from "../repositories/category.respository";
 
 interface ICategory {
-    name: string;
+  name: string;
 }
 
 const categoriesToPreLoad: ICategory[] = [
-    { name: 'Smartphones' },
-    { name: 'Laptops' },
-    { name: 'Tablets' },
-    { name: 'Headphones' },
-    { name: 'Cameras' },
-    { name: 'Printers' },
-    { name: 'Monitors' },
-    { name: 'Storage' },
-    { name: 'Accessories' }
+  { name: "Electric Basses" },
+  { name: "Guitars" },
+  { name: "Drum Kits" },
+  { name: "Guitar Picks" },
+  { name: "Drum Sticks" },
+  { name: "String Sets" },
 ];
 
 export const preLoadCategories = async () => {
-    const categories = await CategoryRepository.find();
-    if (!categories.length) await AppDataSource.createQueryBuilder().insert().into(Category).values(categoriesToPreLoad).execute();
-    console.log('Categories preloaded');
-}
+  const categories = await CategoryRepository.find();
+  if (!categories.length)
+    await AppDataSource.createQueryBuilder()
+      .insert()
+      .into(Category)
+      .values(categoriesToPreLoad)
+      .execute();
+  console.log("Categories preloaded");
+};
--- back/src/helpers/preLoadProducts.ts
+++ back/src/helpers/preLoadProducts.ts
@@ -13,64 +13,207 @@ interface IProduct {
 
 const productsToPreLoad: IProduct[] = [
   {
-    name: "iPhone 11",
-    price: 699,
+    name: "Fender Precision Bass",
+    price: 800,
     description:
-      "Experience power and elegance with the iPhone 11: capture stunning moments with its dual-camera system, enjoy exceptional performance, and immerse yourself in a brilliant Liquid Retina display. Discover a world of possibilities in the palm of your hand!",
+      "Classic electric bass with an alder body and maple neck, perfect for any style.",
     image:
-      "https://www.apple.com/v/iphone-11/a/images/meta/og__f2j3dwkzna2u.png",
+      "https://http2.mlstatic.com/D_NQ_NP_2X_942834-MLA45400121419_032021-F.webp",
     categoryId: 1,
     stock: 10,
   },
   {
-    name: "MacBook Air",
-    price: 999,
+    name: "Fender Jazz Bass",
+    price: 900,
     description:
-      "Embrace efficiency and sophistication with the MacBook Air: lightweight design meets powerful performance, stunning Retina display brings your work to life, and all-day battery life keeps you productive wherever you go. Elevate your computing experience with the MacBook Air.",
+      "Electric bass with dual single-coil pickups for warm, articulate tones—ideal for jazz and funk.",
     image:
-      "https://www.apple.com/v/macbook-air/a/images/meta/og__d5k62k8b4qka.png",
+      "https://blog.musicopolix.com/wp-content/webp-express/webp-images/uploads/2016/04/fender-american-elite-jazz-bass-v-nat_1.jpg.webp",
+    categoryId: 1,
+    stock: 8,
+  },
+  {
+    name: "Music Man StingRay",
+    price: 1800,
+    description:
+      "High-end bass featuring a humbucker pickup and active 3-band EQ preamp.",
+    image:
+      "https://s3-us-west-2.amazonaws.com/static.music-man.com/website/images/instruments/instrument-50.png?1598482639",
+    categoryId: 1,
+    stock: 5,
+  },
+  {
+    name: "Warwick Corvette Standard Fretless",
+    price: 1600,
+    description:
+      "German-made bass with an elm body and MEC electronics for clear, powerful tone.",
+    image:
+      "https://media.guitarcenter.com/is/image/MMGS7/519129000000000-00-600x600.jpg",
+    categoryId: 1,
+    stock: 6,
+  },
+
+  {
+    name: "Classical Nylon Guitar Fender",
+    price: 130,
+    description:
+      "Beginner-friendly nylon-string guitar with a mahogany back and spruce top.",
+    image:
+      "https://www.fmicassets.com/demandware/assets/acoustic-guitars/classical/overview/classical-hero-product.jpg",
     categoryId: 2,
-    stock: 10,
+    stock: 12,
   },
   {
-    name: "iPad Pro",
-    price: 799,
+    name: "Acoustic Steel-String Guitar Yamaha",
+    price: 250,
     description:
-      "Unleash your creativity and productivity with the iPad Pro: powerful performance, stunning Liquid Retina display, and all-day battery life make the iPad Pro the perfect tool for work and play. Transform your ideas into reality with the iPad Pro.",
+      "Steel-string acoustic guitar with a sapele body and rosewood fingerboard.",
     image:
-      "https://www.apple.com/v/ipad-pro/a/images/meta/og__d8m6x7smkntm.png",
+      "https://usa.yamaha.com/files/02_FG9-R_f_0001_87381dd45ce472f1d1e39b29123d7f3d.jpg?impolicy=resize&imwid=2000&imhei=2000",
+    categoryId: 2,
+    stock: 15,
+  },
+  {
+    name: "Fender Stratocaster",
+    price: 1000,
+    description:
+      "Iconic electric guitar with three single-coil pickups and an alder body.",
+    image:
+      "https://acdn-us.mitiendanube.com/stores/003/928/657/products/h97920000001000-02-86x86-d88220e3865e880e7517080230376581-1024-1024.jpg",
+    categoryId: 2,
+    stock: 7,
+  },
+  {
+    name: "Fender Telecaster",
+    price: 1200,
+    description:
+      "Classic electric guitar with two single-coil pickups for bright, cutting tone.",
+    image:
+      "https://acdn-us.mitiendanube.com/stores/001/040/912/products/ef11880a-4400-46bc-9ac6-e53b99b96d40-5fc9c6ae21fcc4947617079209288483-1024-1024.jpg",
+    categoryId: 2,
+    stock: 5,
+  },
+  {
+    name: "PSR Modern Electric Guitar",
+    price: 900,
+    description:
+      "Versatile modern electric guitar suitable for rock, blues, and pop styles.",
+    image:
+      "https://www.kennysmusic.co.uk/images/prs-se-custom-24-electric-guitar-turquoise-b-stock-p7485-25886_medium.jpg",
+    categoryId: 2,
+    stock: 9,
+  },
+
+  {
+    name: "Pearl Roadshow Drum Set",
+    price: 500,
+    description: 'Complete beginner kit with 9" toms and a 14" snare.',
+    image: "https://m.media-amazon.com/images/I/81Dsq8-JU0L._AC_SL1500_.jpg",
     categoryId: 3,
-    stock: 10,
+    stock: 4,
   },
   {
-    name: "Apple Watch Series 6",
-    price: 399,
+    name: "Yamaha Stage Custom",
+    price: 700,
     description:
-      "Stay connected and healthy with the Apple Watch Series 6: track your workouts, monitor your health, and stay in touch with the people and information you care about most. Experience the future of health and wellness with the Apple Watch Series 6.",
+      "Intermediate-level kit with birch shells and included hardware.",
     image:
-      "https://www.apple.com/v/apple-watch-series-6/a/images/meta/og__c1zv8c8n7q06.png",
+      "https://es.yamaha.com/es/files/SCB_DUS_a_0002_7c990cac1dee48790f654fe60c06bea8.jpg?impolicy=resize&imwid=2000&imhei=800",
+    categoryId: 3,
+    stock: 3,
+  },
+  {
+    name: "Tama Imperialstar",
+    price: 780,
+    description: "Durable mid-level set with chrome hardware and resin heads.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyvA7fSXvIxzuCIs9vPg88lttj95frsImcSQ&s",
+    categoryId: 3,
+    stock: 2,
+  },
+
+  {
+    name: "Delrin Standard Pick 0.73mm",
+    price: 1,
+    description: "Medium-gauge Delrin pick for excellent grip and durability.",
+    image:
+      "https://intermusicashop.com.ar/wp-content/uploads/2024/11/Puas_Medium_Martin_Delrin_0.73mm_X_12_Unidades_D_947654-MLA53226320944_012023-O.jpg",
     categoryId: 4,
-    stock: 10,
+    stock: 200,
   },
   {
-    name: "AirPods Pro",
-    price: 249,
-    description:
-      "Immerse yourself in sound with the AirPods Pro: active noise cancellation, transparency mode, and customizable fit make the AirPods Pro the perfect companion for music, calls, and everything in between. Elevate your audio experience with the AirPods Pro.",
+    name: "Celluloid Classic Pick 0.88mm",
+    price: 1,
+    description: "Traditional celluloid pick with a warm tone and smooth feel.",
     image:
-      "https://www.apple.com/v/airpods-pro/a/images/meta/og__c1zv8c8n7q06.png",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9pNgQ6YwkbbfWYXNjfwwfFOK4bVDui2-PIw&s",
+    categoryId: 4,
+    stock: 180,
+  },
+  {
+    name: "Nylon Jazz Pick 1.20mm",
+    price: 1,
+    description: "Heavy nylon pick for defined attack and long lifespan.",
+    image:
+      "https://magmastoreusa.com/cdn/shop/products/PT120_1200x1200.jpg?v=1659028223",
+    categoryId: 4,
+    stock: 150,
+  },
+
+  {
+    name: "Vic Firth 5A American Classic",
+    price: 13,
+    description: "Balanced sticks suitable for all musical styles.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyi1EvOmCb53GM9bntPtJbEGQbn-Er9J-XJQ&s",
     categoryId: 5,
-    stock: 10,
+    stock: 80,
   },
   {
-    name: "HomePod mini",
-    price: 99,
-    description:
-      "Elevate your home audio experience with the HomePod mini: immersive sound, intelligent assistant, and smart home hub make the HomePod mini the perfect addition to your home. Enjoy a world of music, news, and more with the HomePod mini.",
+    name: "Zildjian 5B Drumsticks",
+    price: 15,
+    description: "Slightly thicker sticks for extra power and durability.",
     image:
-      "https://www.apple.com/v/homepod-mini/a/images/meta/og__d5k62k8b4qka.png",
+      "https://zildjian.com/cdn/shop/files/z5b-5b-select-hickory-natural-pair.jpg?v=1737581552&width=1445",
+    categoryId: 5,
+    stock: 70,
+  },
+
+  {
+    name: "Ernie Ball Super Slinky Bass Strings",
+    price: 20,
+    description: "Light gauge bass set ideal for slap and fast-playing styles.",
+    image: "https://example.com/images/strings-bass-slinky.jpg",
     categoryId: 6,
-    stock: 10,
+    stock: 30,
+  },
+  {
+    name: "D'Addario Pro-Arte Nylon Strings",
+    price: 10,
+    description:
+      "Premium nylon strings for classical guitar with balanced tone.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTRbn0lNk2o5nw1Lkzl_Kk05q9NnmTNgl_qA&s",
+    categoryId: 6,
+    stock: 40,
+  },
+  {
+    name: "Ernie Ball Regular Slinky Electric Strings",
+    price: 10,
+    description: "Classic steel strings for electric guitar with bright tone.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRTsNoHnZpXdsjC4i7zmT8OOecP8IGmtEMIQ&s",
+    categoryId: 6,
+    stock: 50,
+  },
+  {
+    name: "Magma BE170N Nickel Plated Steel Electric Bass Strings",
+    price: 20,
+    description:
+      "Coated phosphor bronze strings for acoustic guitar with extended life.",
+    image: "https://www.stringsbymail.com/images/BE170N.jpg",
+    categoryId: 6,
+    stock: 45,
   },
 ];
 
