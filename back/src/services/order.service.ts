2c98df7e5529f7e8506401d90d5d52ca8bed6cb3
AleFalces <alefalces@gmail.com>
2025-04-23 17:38:37 -0300

    createOrder

--- back/src/services/order.service.ts
+++ back/src/services/order.service.ts
@@ -7,9 +7,11 @@ import { UserRepository } from "../repositories/user.repository";
 export const createOrderService = async (
   createOrderDto: CreateOrderDto
 ): Promise<Order> => {
+  const uniqueProductIds = Array.from(new Set(createOrderDto.products));
+
   const productsF = [];
 
-  for await (const id of createOrderDto.products) {
+  for await (const id of uniqueProductIds) {
     const product = await ProductRepository.findOneBy({ id });
     if (!product) throw new Error("Product not found");
     productsF.push(product);
