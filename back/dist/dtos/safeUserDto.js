2dd40b86ea77e5ce4b91dd18f1cfb75834409fa5
AleFalces <alefalces@gmail.com>
2025-04-28 17:43:58 -0300

    package

---
+++ back/dist/dtos/safeUserDto.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "admin";
+    Role["USER"] = "user";
+})(Role || (Role = {}));
